d4c693e792256936767a4ea1c1da0384ff698722
gsenayildiz <ggulsenayildiz@gmail.com>
2024-12-05 14:30:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## SHOES-APP
+- 
+Bu proje, kullanıcıların ayakkabı ürünlerini filtreleyerek görüntüleyebildiği bir e-ticaret uygulaması niteliği taşır. React kullanılarak geliştirilen projede, ürün verileri bir REST API (JSON Server) üzerinden alınır.
+
+- Proje, hem kullanıcı dostu bir filtreleme sistemi sunarken hem de modern web geliştirme araçlarının bir arada nasıl kullanılacağını gösterir.
+
+# Teknolojiler
+
+- react
+- typescript
+
+# Kütüphaneler
+
+- react-router-dom
+- tailwind
+- axios
+- json-server
+- @tanstack/react-query
+- react-toastify
+
+- <img src="/public/screen.gif"/>
---
+++ src/api/index.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import formatParams from "../utils/formatParams";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+
+
+// urlden gelen parametre nesnesi bu şekilde
+const urlParams = {
+  color: "blue,light-gray",
+  gender: "men",
+  size: "40,41",
+  price: "371",
+};
+
+const paramsStr = formatParams(urlParams);
+
+// api'a gönderilcek parametre stringi bu şekilde
+const url =
+  "?color_like=blue&color_like=light-gray&size_like=40&size_like=41&gender=men&price_lte=371";
+
+// bütün ayakkabıları al
+export const getShoes = (params: string) =>
+  api.get(`/shoes${params}`).then((res) => res.data);
+
+// bir ayakkabıyı al
+export const getShoe = (id: string) =>
+  api.get(`/shoes/${id}`).then((res) => res.data);
